f02c566d11baa336d641569ed2a485d8b8cfc84f
minnasu <minnasu@tencent.com>
2024-03-31 18:07:31 +0800

    update

---
+++ .history/src/components/Button_20240330023438.tsx
@@ -0,0 +1,250 @@
+import React, { CSSProperties, useMemo } from 'react'
+import styled from 'styled-components'
+// import { IconLoading } from './Icon';
+import Pop, { PopProps } from './Pop';
+
+export type ButtonProps = {
+  /* 按钮主要文案 */
+  text?: string;
+  /* 按钮次要文案 */
+  subText?: string;
+  /* 按钮图标: 若仅指定 icon 而未指定文案，则渲染为按钮图标（宽高一致） */
+  icon?: React.ReactNode;
+  /* 设置按钮宽度&高度：设置后 padding 将不起作用 */
+  width?: string;
+  height?: string;
+  /* 按钮文字对齐方式 */
+  justify?: "center" | "start" | "end";
+  /* 按钮风格 */
+  type?: "text" | "background" | "border" | "line";
+  /* 按钮状态 */
+  status?: "default" | "primary" | "warning" | "danger" | "success";
+  /* 是否禁用 */
+  disabled?: boolean;
+  /* 是否记载中 */
+  loading?: boolean;
+  /* 按钮气泡 */
+  pop?: React.ReactNode | string;
+  /* 气泡配置 */
+  PopProps?: PopProps;
+  /* 跳转链接：按钮作为 a 标签 */
+  href?: string;
+  /* a 标签对应的 target 属性 */
+  target?: "_self" | "_blank" | "_top" | "_parent";
+  /* 按钮点击事件 */
+  onClick?: (e: React.UIEvent) => void;
+  style?: CSSProperties;
+  className?: string;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  subText,
+  icon,
+  width,
+  height,
+  justify = "center",
+  type = "border",
+  status = "default",
+  disabled,
+  // loading,
+  pop,
+  PopProps,
+  href,
+  target = "_blank",
+  onClick,
+  style,
+  className = "",
+}) => {
+  /* 仅指定 icon 属性时 iconOnly 默认为true */
+  const iconOnly = useMemo(() => {
+    if (icon) {
+      if (!text && !subText) return true;
+    } else {
+      return false;
+    }
+  }, [icon, text, subText]);
+
+  // 配置按钮颜色
+  const buttonColorConfig = useMemo(() => {
+    switch (status) {
+      case "success":
+        return "var(--color-green-";
+      case "warning":
+        return "var(--color-orange-";
+      case "danger":
+        return "var(--color-red-";
+      case "primary":
+        return "var(--color-primary-";
+      default:
+        return "";
+    }
+  }, [status]);
+  return (
+    <>
+      {!href ? (
+        <StyledButton
+          className={`land-button ${type} ${status} ${iconOnly ? "iconOnly" : ""
+            } ${disabled ? "disabled" : ""} ${pop ? "hover-pop" : ""
+            } ${className}`}
+          style={style}
+          width={width}
+          height={height}
+          justify={justify}
+          color={buttonColorConfig}
+          onClick={(e: React.UIEvent) => onClick?.(e)}
+        >
+          {loading ? <>load</> : icon}
+          {!iconOnly && (
+            <div>
+              <span>{text}</span>
+              {subText && <span className="subText">{subText}</span>}
+            </div>
+          )}
+          {pop && <Pop content={pop} {...PopProps} />}
+        </StyledButton>
+      ) : (
+        <StyleButtonLink
+          href={href}
+          target={target}
+          className={`land-button ${type} ${status} ${iconOnly ? "iconOnly" : ""
+            } ${disabled ? "disabled" : ""} ${pop ? "hover-pop" : ""
+            } ${className}`}
+          style={style}
+          width={width}
+          height={height}
+          justify={justify}
+          color={buttonColorConfig}
+          onClick={(e: React.UIEvent) => onClick?.(e)}
+        >
+          {/* {loading ? <IconLoading /> : icon} */}
+          {!iconOnly && (
+            <div>
+              <span>{text}</span>
+              <span className="subText">{subText}</span>
+            </div>
+          )}
+          {pop && <Pop content={pop} {...PopProps} />}
+        </StyleButtonLink>
+      )}
+    </>
+  );
+};
+
+const StyledButton = styled.div<{
+  justify?: any;
+  color?: string;
+  width?: string;
+  height?: string;
+}>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => props.justify};
+  gap: var(--gap-8);
+  padding: ${(props) =>
+    props.width && props.width !== "100%" ? "" : "var(--padding-medium)"};
+  width: ${(props) => (props.width ? props.width : "fit-content")};
+  height: ${(props) => (props.height ? props.height : "fit-content")};
+  color: ${(props) =>
+    props.color ? `${props.color}6)` : "var(--color-text-2)"};
+  &:hover {
+    color: ${(props) =>
+    props.color ? `${props.color}5)` : "var(--color-text-3)"};
+  }
+  &:active {
+    color: ${(props) =>
+    props.color ? `${props.color}7)` : "var(--color-text-1)"};
+  }
+  font-size: var(--font-content-medium);
+  border-radius: var(--radius-6);
+  box-sizing: border-box;
+  transition: all var(--transition-15) linear;
+  cursor: pointer;
+  .subText {
+    margin-left: 4px;
+    font-size: var(--font-content-small);
+    opacity: var(--opacity-68);
+  }
+
+  &.text,
+  &.border.default {
+    &:hover {
+      background-color: var(--color-bg-1);
+    }
+    &:active {
+      background-color: var(--color-bg-2);
+    }
+  }
+
+  &.background {
+    color: var(--color-text-white);
+    background-color: ${(props) => `${props.color}6)`};
+    &.default {
+      background-color: var(--color-text-2);
+      &:hover {
+        background-color: var(--color-text-1);
+      }
+      &:active {
+        background-color: var(--color-text-1);
+      }
+    }
+    &:hover {
+      background-color: ${(props) => `${props.color}5)`};
+    }
+    &:active {
+      background-color: ${(props) => `${props.color}7)`};
+    }
+  }
+
+  &.border {
+    padding: 7px 15px;
+    border: ${(props) => `var(--border-1) solid ${props.color}4)`};
+    &.default {
+      border-color: var(--color-border-2);
+    }
+    &:hover {
+      background-color: ${(props) => `${props.color}1)`};
+    }
+    &:active {
+      background-color: ${(props) => `${props.color}2)`};
+    }
+  }
+
+  &.line {
+    border-bottom: var(--border-1) solid transparent;
+    border-color: ${(props) => `${props.color}4)`};
+    border-radius: 0;
+    &.default {
+      border-color: var(--color-text-2);
+    }
+    &:hover {
+      border-color: ${(props) => `${props.color}5)`};
+    }
+    &:active {
+      border-color: ${(props) => `${props.color}7)`};
+    }
+  }
+
+  &.iconOnly {
+    padding: 0;
+    width: 36px;
+    height: 36px;
+  }
+  &.disabled {
+    opacity: var(--opacity-68);
+    cursor: default;
+  }
+`;
+
+const StyleButtonLink = styled.a<{
+  justify?: any;
+  color?: string;
+  width?: string;
+  height?: string;
+}>`
+  ${StyledButton};
+  text-decoration: none;
+`;
+
+export default Button
---
+++ .history/src/components/ColorPicker copy_20240330011936.tsx
@@ -0,0 +1,457 @@
+import React, { useState, useMemo, useCallback, useEffect } from "react";
+import styled from "styled-components";
+import Input from "./Input";
+import tinycolor from 'tinycolor2'
+import Flex from "./Flex";
+
+const COLOR_DATA = [
+  [
+    { id: 1, color: '#e9e9e9' },
+    { id: 2, color: '#d9d9d9' },
+    { id: 3, color: '#c4c4c4' },
+    { id: 4, color: '#9d9d9d' },
+  ],
+  [
+    { id: 5, color: '#7b7b7b' },
+    { id: 6, color: '#5c5c5c' },
+    { id: 7, color: '#333333' },
+    { id: 8, color: '#262626' },
+  ],
+  [
+    { id: 9, color: '#efccbd' },
+    { id: 10, color: '#d39f9d' },
+    { id: 11, color: '#c4726d' },
+    { id: 12, color: '#ac3526' },
+  ],
+  [
+    { id: 13, color: '#f6e3bd' },
+    { id: 14, color: '#ebbd6f' },
+    { id: 15, color: '#df964a' },
+    { id: 16, color: '#c76336' },
+  ],
+  [
+    { id: 17, color: '#fbf2d4' },
+    { id: 18, color: '#f7e7ab' },
+    { id: 19, color: '#f4db86' },
+    { id: 20, color: '#ebbc58' },
+  ],
+  [
+    { id: 21, color: '#dee9d7' },
+    { id: 22, color: '#bdd992' },
+    { id: 23, color: '#9dbe56' },
+    { id: 24, color: '#758d3a' },
+  ],
+  [
+    { id: 25, color: '#e7f6fd' },
+    { id: 26, color: '#9dd9f8' },
+    { id: 27, color: '#81bde5' },
+    { id: 28, color: '#4f84eb' },
+  ],
+  [
+    { id: 29, color: '#cdc7ee' },
+    { id: 30, color: '#877cda' },
+    { id: 31, color: '#4249ac' },
+    { id: 32, color: '#313fa4' },
+  ],
+  [
+    { id: 33, color: '#f6e6fb' },
+    { id: 34, color: '#d69aeb' },
+    { id: 35, color: '#b253d3' },
+    { id: 36, color: '#8a31c4' },
+  ],
+];
+
+type ColorProps = {
+  /* 外部回传的 value 值 */
+  value?: string;
+  size?: string | number;
+  showDrop?: boolean;
+  onChange?: (color: string) => void;
+};
+const ColorPicker: React.FC<ColorProps> = ({
+  value = "#ffffff",
+  size = 48,
+  showDrop = true,
+  onChange,
+}) => {
+  const [show, setShow] = useState<boolean>(false);
+  // 当前的 hex 值
+  const [currentColor, setCurrentColor] = useState<string>(
+    !value
+      ? "#ffffff"
+      : value === "transparent"
+      ? "transparent"
+      : tinycolor(value).toString()
+  );
+  const [inputColor, setInputColor] = useState<string>(
+    currentColor.split("#")[1]
+  );
+  const [opacity, setOpacity] = useState<number>(100);
+  const [h, setH] = useState<number>(0);
+  const [s, setS] = useState<number>(0);
+  const [v, setV] = useState<number>(0);
+  useEffect(() => {
+    if (!value) return;
+    if (value === "transparent") {
+      setCurrentColor("transparent");
+    } else {
+      const { h, s, v } = tinycolor(`#${value}`).toHsv();
+      setH(h);
+      !move && setS(s);
+      !move && setV(v);
+      setCurrentColor(value);
+      setInputColor(`${value.split("#")[1]}`);
+    }
+  }, [value]);
+  // 获取 rgba 颜色值
+  const getRgbaColor = useCallback((color: string, opacity = 100) => {
+    if (color !== "transparent") {
+      const c = tinycolor(color);
+      const { r, g, b } = c.toRgb();
+      return `rgba(${r}, ${g}, ${b}, ${opacity / 100})`;
+    }
+    return color;
+  }, []);
+  const currentRgbColor = useMemo(() => {
+    return getRgbaColor(currentColor, opacity);
+  }, [currentColor, opacity]);
+
+  const handleHexInputChange = (val: string) => {
+    setInputColor(val);
+    // const rgb = tinycolor(`#${val}`).toRgb();
+    setCurrentColor(`#${val}`);
+    const hsv = tinycolor(`#${val}`).toHsv();
+    setH(hsv.h);
+    setS(hsv.s);
+    setV(hsv.v);
+  };
+  // 设置渐变面板 sv 背景颜色
+  const svColor = useMemo(() => {
+    const hex = tinycolor(`hsv(${h},${1},${1})`).toHex();
+    return `#${hex}`;
+  }, [h]);
+  // H 值 Slider改变
+  const handleSliderChange = (val: string) => {
+    setH(Number(val));
+    const hex = tinycolor(`hsv(${val},${s},${v})`).toHex();
+    setInputColor(hex);
+    setCurrentColor(`#${hex}`);
+    onChange?.(getRgbaColor(currentColor, opacity / 100));
+  };
+
+  // 拖动选择颜色
+  const [move, setMove] = useState<Boolean>(false);
+  const handleThumb = (e: any) => {
+    const panel = e.target.getBoundingClientRect();
+    const diffX = e.clientX - panel.left;
+    const diffY = e.clientY - panel.top;
+    if (
+      diffX == 0 ||
+      diffX == panel.width ||
+      diffY == 0 ||
+      diffY == panel.height
+    ) {
+      setMove(false);
+    }
+    setS(diffX / 180);
+    setV(1 - diffY / 86);
+    const hex = tinycolor(`hsv(${h},${s},${v})`).toHex();
+    setInputColor(hex);
+    setCurrentColor(`#${hex}`);
+    onChange?.(getRgbaColor(currentColor, opacity));
+  };
+  return (
+    <StyledColorPicker
+      className="land-color-picker"
+      color={value}
+      size={typeof size === "string" ? size : `${size}px`}
+      onClick={() => setShow(!show)}
+    >
+      <div className="land-color-trigger">
+        <div
+          className="land-color-grid"
+          style={{ background: currentRgbColor }}
+        ></div>
+        <Input
+          prefix="#"
+          maxLength={6}
+          value={inputColor}
+          onChange={(val) => handleHexInputChange(val)}
+          onBlur={() => onChange?.(currentRgbColor)}
+          style={{
+            height: 24,
+            fontSize: "12px",
+            border: "none",
+            gap: 0,
+            padding: 0,
+          }}
+        />
+      </div>
+      {showDrop && (
+        <StyledColorPanel
+          className={`land-color-drop ${show ? "show" : ""}`}
+        >
+          <StyledColorGrid
+            className="StyledColorGrid"
+            svColor={svColor}
+            onMouseMove={(e: any) => move && handleThumb(e)}
+            onMouseDown={(e: any) => {
+              e.preventDefault();
+              handleThumb(e);
+              setMove(true);
+            }}
+            onMouseUp={() => {
+              setMove(false);
+            }}
+          >
+            <div
+              className="color-thumb"
+              style={{ left: s * 180, top: 86 - v * 86 }}
+            ></div>
+          </StyledColorGrid>
+          <Flex gap={8}>
+            <div
+              className="radius-4 border"
+              style={{
+                width: "40px",
+                aspectRatio: 1,
+                flexShrink: 0,
+                background: currentRgbColor,
+              }}
+            ></div>
+            <Flex column gap={8}>
+              <StyledColorSlider
+                type="range"
+                max={360}
+                value={h}
+                step={1}
+                currentColor={`#${inputColor}`}
+                onChange={(e) => handleSliderChange(e.target.value)}
+              />
+              {true && (
+                <StyledOpacityWrap>
+                  <StyledColorSlider
+                    type="range"
+                    step={1}
+                    max={100}
+                    value={opacity}
+                    className="opacity"
+                    currentColor={svColor}
+                    onChange={(e: any) => {
+                      setOpacity(e.target.value);
+                      onChange?.(
+                        getRgbaColor(currentColor, e.target.value / 100)
+                      );
+                    }}
+                  />
+                </StyledOpacityWrap>
+              )}
+            </Flex>
+          </Flex>
+          <div className="flex gap-8">
+            <Input
+              className="flex-2"
+              prefix="#"
+              maxLength={6}
+              value={inputColor.split("#")[0]}
+              onChange={(val) => handleHexInputChange(val)}
+            />
+            <Input
+              className="flex-1"
+              max={100}
+              min={0}
+              value={String(opacity)}
+              onChange={(val) => {
+                setOpacity(Number(val));
+                onChange?.(getRgbaColor(currentColor, Number(val) / 100));
+              }}
+            />
+          </div>
+          <div className="width-100 flex" style={{ gap: 2 }}>
+            {COLOR_DATA?.map((list, idx) => (
+              <div className="flex column" key={idx} style={{ gap: 2 }}>
+                {list.map((item) => (
+                  <StyleColorItem
+                    onClick={() => {
+                      !value && handleHexInputChange(item.color.split("#")[1]);
+                      setCurrentColor(item.color);
+                    }}
+                    // @ts-ignore
+                    style={{ "--tacc-color-item-bgColor": item.color }}
+                    key={item.id}
+                  />
+                ))}
+              </div>
+            ))}
+          </div>
+        </StyledColorPanel>
+      )}
+    </StyledColorPicker>
+  );
+};
+
+const StyledColorPicker = styled.div<{
+  color?: string;
+  size?: string;
+}>`
+  position: relative;
+  width: ${(props) => props.size};
+  cursor: pointer;
+  .land-color-trigger {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    width: 100%;
+  }
+  .land-color-grid {
+    width: 100%;
+    aspect-ratio: 1;
+    background-color: ${props => props.color};
+    border: 8px solid var(--color-bg-1);
+    border-radius: 4px;
+    transition: border-color var(--transition-15) linear;
+    will-change: border-color;
+    &:hover {
+      border-color: var(--color-bg-2);
+    }
+    &:active {
+      border-color: var(--color-bg-3);
+    }
+  }
+`;
+
+const StyledColorPanel = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  background-color: var(--color-bg-white);
+  width: 204px;
+  border-radius: 4px;
+  box-shadow: var(--boxshadow-medium);
+  opacity: 0;
+  pointer-events: none;
+  transform: translateY(8px);
+  transition: opacity var(--transition-15) linear;
+  &.show {
+    opacity: 1;
+    pointer-events: all;
+  }
+`;
+
+const StyledColorGrid = styled.div<{
+  svColor?: string;
+}>`
+  position: relative;
+  width: 100%;
+  height: 86px;
+  border-radius: 4px;
+  background-color: ${(props) => props.svColor};
+  background-image: linear-gradient(0deg, rgb(0, 0, 0), transparent),
+    linear-gradient(90deg, rgb(255, 255, 255), rgba(255, 255, 255, 0));
+    overflow: hidden;
+  .color-thumb {
+    position: absolute;
+    width: 12px;
+    height: 12px;
+    border: 1px solid var(--color-border-2);
+    border-radius: 4px;
+    transform: translate(-50%,-50%);
+    pointer-events: none;
+  }
+`;
+const StyledColorSlider = styled.input<{
+  currentColor?: string;
+}>`
+  appearance: none;
+  -webkit-appearance: none;
+  margin: 0;
+  width: 132px;
+  height: 16px;
+  border: 0px;
+  outline: none;
+  object-fit: contain;
+  border-radius: 4px;
+  background: linear-gradient(
+    to right,
+    #ff0000,
+    #ffd600,
+    #52ff00,
+    #00fff0,
+    #0500ff,
+    #c000ff,
+    #ff0000
+  );
+  &.opacity {
+    position: absolute;
+    background: ${(props) =>
+    `linear-gradient(to right, rgba(0,0,0,0), ${props.currentColor})`};
+    &::-webkit-slider-thumb {
+      background: transparent;
+    }
+  }
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 16px;
+    height: 16px;
+    border-radius: 4px;
+    background: ${(props) => props.currentColor};
+    border: 1px solid var(--color-border-1);
+    outline: 1px solid var(--color-border-3);
+    &:hover {
+      cursor: ew-resize;
+    }
+  }
+`;
+
+const StyledOpacityWrap = styled.div`
+  padding: relative;
+  width: 132px;
+  height: 16px;
+  border-radius: 4px;
+  background-image: conic-gradient(
+    var(--color-bg-2) 0 25%,
+    transparent 0 50%,
+    var(--color-bg-2) 0 75%,
+    transparent 0
+  );
+  background-size: 8px 8px;
+`;
+
+const StyleColorItem = styled.div`
+  border: var(--tacc-color-item-border, var(--spaui-input-border));
+  border-radius: var(--tacc-color-item-radius, 4px);
+  background-color: var(--tacc-color-item-bgColor, transparent);
+  width: 18px;
+  height: 18px;
+  transition: all 0.2s;
+  &:hover {
+    --tacc-color-item-border: 1px solid var(--od-gray-05);
+  }
+  &.active {
+    --tacc-color-item-border: 1px solid var(--od-blue-06);
+  }
+  cursor: pointer;
+  &.null {
+    position: relative;
+    background-color: transparent;
+    &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 10px;
+      top: -4px;
+      width: 2px;
+      height: 30px;
+      border-radius: 1px;
+      background-color: var(--od-red-06);
+      transform: rotate(-45deg);
+    }
+  }
+`;
+
+export default ColorPicker;
