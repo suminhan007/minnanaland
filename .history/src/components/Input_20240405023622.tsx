4c1d4206ab812aed4dc14b2d089f51d6a6def095
minnasu <minnasu@tencent.com>
2024-04-05 02:44:10 +0800

    colorpicker:修复uploader自适应

---
+++ .history/src/components/Input_20240405023622.tsx
@@ -0,0 +1,84 @@
+import React, { CSSProperties } from "react";
+import styled from "styled-components";
+
+type Props = {
+  type?: string;
+  value?: string;
+  placeholder?: string;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  w?: number | string;
+  max?: number;
+  min?: number;
+  onChange?: (val: string) => void;
+  className?: string;
+  style?: CSSProperties;
+  [key: string]: any;
+};
+const Input: React.FC<Props> = ({
+  type = "text",
+  value,
+  placeholder,
+  prefix,
+  suffix,
+  w,
+  min,
+  max,
+  onChange,
+  className,
+  wrapStyle,
+  ...restProps
+}) => {
+  return (
+    <StyleInputWrap
+      className={`land-input ${className}`}
+      style={{ width: typeof w === "number" ? `${w}px` : w, ...wrapStyle }}
+    >
+      {prefix}
+      <input
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        min={min}
+        max={max}
+        onChange={(e) => onChange?.(e.target.value)}
+        {...restProps}
+      />
+      {suffix}
+    </StyleInputWrap>
+  );
+};
+
+const StyleInputWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: var(--gap-4);
+  padding: 0 4px;
+  border: var(--border-1) solid var(--color-border-1);
+  border-radius: var(--radius-4);
+  input {
+    width: 100%;
+    height: 36px;
+    background-color: transparent;
+    appearance: none;
+    -moz-appearance: none;
+    border: none;
+    outline: none;
+    caret-color: var(--color-primary-6);
+    &:focus-within,
+    &:focus,
+    &:focus-visible,
+    &:active {
+      border: none;
+      box-shadow: none;
+      background-color: transparent;
+    }
+    &::placeholder {
+      color: var(--color-text-3);
+    }
+    &::selection {
+      background-color: var(--color-primary-2);
+    }
+  }
+`;
+export default Input;
---
+++ .history/src/components/Pop_20240405014539.tsx
@@ -0,0 +1,70 @@
+import React, { CSSProperties } from 'react';
+import styled from 'styled-components';
+
+enum PopType {
+    BG = 'background',
+    BORDER = 'border',
+}
+
+export type PopProps = {
+  type?: PopType;
+  theme?: "light" | "dark";
+  content?: React.ReactNode | string;
+  style?: CSSProperties;
+  className?: string;
+};
+
+const Pop: React.FC<PopProps> = ({
+  content,
+  theme = "light",
+  style,
+  className = "",
+}) => {
+  return (
+    <StyledBtnPop className={`land-pop ${theme} ${className}`} style={style}>
+      {content}
+      <div className="land-pop-arrow"></div>
+    </StyledBtnPop>
+  );
+};
+
+const StyledBtnPop = styled.div`
+  position: absolute;
+  padding: 8px 12px;
+  width: max-content;
+  bottom: 100%;
+  left: 50%;
+  font-size: 12px;
+  color: var(--color-text-3);
+  border-radius: 6px;
+  background-color: var(--color-bg-white);
+  box-shadow: var(--boxshadow-normal);
+  border: var(--border-1) solid var(--color-border-1);
+  transform: translate(-50%, 50%);
+  /* opacity: 0; */
+  transform-origin: bottom center;
+  pointer-events: none;
+  transition: all 0.15s linear;
+  .land-pop-arrow {
+    display: block;
+    position: absolute;
+    left: 50%;
+    bottom: -50%;
+    transform: translate(-50%, -80%) rotate(45deg);
+    width: 12px;
+    height: 12px;
+    border-right: var(--border-1) solid var(--color-border-1);
+    border-bottom: var(--border-1) solid var(--color-border-1);
+    border-bottom-right-radius: var(--radius-4);
+    background-color: var(--color-bg-white);
+  }
+  &.dark {
+    background-color: var(--color-text-2);
+    color: var(--color-text-light);
+    .land-pop-arrow {
+      background-color: var(--color-text-2);
+    }
+  }
+`;
+
+export default Pop
