105f8261f632b964d9325a06a56a40f42946d98e
minnasu <minnasu@tencent.com>
2024-04-17 18:45:51 +0800

    refactor:3d

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Banner from "./scene/Banner";
 type Props = {};
-const Home: React.FC<Props> = ({}) => {
+const Home: React.FC<Props> = ({ }) => {
   return (
     <div>
       <Banner />
