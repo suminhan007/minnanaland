2d21bd6969652fead853ee5ec2f1309eae92ec7a
minnasu <minnasu@tencent.com>
2024-11-20 21:01:24 +0800

    perf: 优化

--- apps/refactor-helper/src/pages/Create/Prompt/index.tsx
+++ apps/refactor-helper/src/pages/Create/Prompt/index.tsx
@@ -1,14 +1,75 @@
-import { LandInput } from '@suminhan/land-design';
-import React from 'react'
+/* 步骤：
+  ······1. 简要描述
+  ······2.
+ */
+import { LandDivider, LandFlex, LandInput, LandRadio, LandSlider, LandTitle } from '@suminhan/land-design';
+import React, { useState } from 'react'
 
 type Props = {
 };
 
 const Prompt: React.FC<Props> = ({
 }) => {
+  const [input, setInput] = useState<string>('');
+  const [radio, setRadio] = useState<number | string>(0);
+  const [similarity, setSimilarity] = useState<number>(5);
   return (
-    <div className='flex both-center width-100 height-100'>
-      <LandInput />
+    <div className='flex column both-center px-24 pt-32 pb-24 mx-auto width-100 height-100 border-box' style={{ maxWidth: '800px' }}>
+      <LandTitle title='step1: 原型图' type='h3' />
+      <LandDivider margin={32} />
+      <LandFlex column gap={24}>
+        <LandFlex column gap={12}>
+          <LandTitle title='1. 模块描述' type='p' />
+          <LandInput
+            placeholder='例如：一个左右切换浏览的【成功案例】模块'
+            value={input}
+            onChange={val => setInput(val)}
+            onClear={() => setInput('')}
+            width='100%'
+            type='background'
+          />
+        </LandFlex>
+
+        <LandFlex column gap={12}>
+          <LandTitle title='2. 布局限制' type='p' />
+          <LandRadio data={[
+            { value: 0, label: '不限' },
+            { value: 1, label: '中心布局' },
+            { value: 2, label: '左右布局' },
+            { value: 3, label: '上下布局' },
+            { value: 4, label: 'grid 布局' },
+            { value: 5, label: 'swiper 布局' },
+            {
+              value: 6, label: <>其他布局<LandInput
+                placeholder='例如：田字格'
+                value={input}
+                onChange={val => setInput(val)}
+                onClear={() => setInput('')}
+                width='100%'
+                type='background'
+              /></>
+            },
+          ]}
+            checked={radio}
+            onChange={val => setRadio(val.value)}
+          />
+          <LandInput
+            placeholder='例如：每行 3 个，共 2 行'
+            prefix='其他：'
+            value={input}
+            onChange={val => setInput(val)}
+            onClear={() => setInput('')}
+            width='100%'
+            type='background'
+          />
+        </LandFlex>
+
+        <LandFlex column gap={12}>
+          <LandTitle title='5. 限制程度' type='p' info='限制程度越高' />
+          <LandSlider min={0} max={10} value={similarity} onChange={val => setSimilarity(val)} />
+        </LandFlex>
+
+      </LandFlex>
     </div>
   )
 }
--- apps/refactor-helper/src/pages/Create/index.tsx
+++ apps/refactor-helper/src/pages/Create/index.tsx
@@ -7,10 +7,10 @@ import { LandAffixContainer, LandButton } from "@suminhan/land-design";
 
 export default function Create() {
   const stepsData = [
-    { id: 1, element: <Prompt /> },
-    { id: 2, element: <StyleConfig /> },
-    { id: 3, element: <TransitionConfig /> },
-    { id: 4, element: <Edit /> }
+    { id: 1, element: <Prompt />, placeholder: '1.结构描述' },
+    { id: 2, element: <StyleConfig />, placeholder: '2.风格配置' },
+    { id: 3, element: <TransitionConfig />, placeholder: '3.交互配置' },
+    { id: 4, element: <Edit />, placeholder: '4.编辑' }
   ]
   const lastIndex = useMemo(() => stepsData.length - 1, [stepsData]);
   const [step, setStep] = useState<number>(0);
@@ -18,10 +18,11 @@ export default function Create() {
     <LandAffixContainer className="flex height-100"
       rbOption={{
         content: <div className='flex gap-12 transition'>
-          <LandButton type='background' onClick={() => step > 0 && setStep(step - 1)} disabled={step === 0}>上一步</LandButton>
-          <LandButton type='background' onClick={() => step < lastIndex && setStep(step + 1)} disabled={step === lastIndex}>下一步</LandButton>
+          <LandButton width="80px" type='background' className={`${step === 0 ? 'opacity-0 evens-none' : ''} transition`} onClick={() => step > 0 && setStep(step - 1)} disabled={step === 0}>上一步</LandButton>
+          <LandButton width="80px" type='background' status={step === lastIndex ? 'primary' : 'default'} onClick={() => step < lastIndex && setStep(step + 1)} >{step === lastIndex ? '完成' : '下一步'}</LandButton>
         </div>,
-        gap: 20
+        gap: 20,
+        custom: true,
       }}
       tcOption={{
         content: <div className="flex gap-12">
@@ -30,17 +31,19 @@ export default function Create() {
             className="radius-50 transition"
             style={{ width: '8px', height: '8px', backgroundColor: step === idx ? 'var(--color-primary-6)' : 'var(--color-primary-2)' }}
           ></div>)}
-        </div>
+        </div>,
+        custom: true,
       }}
     >
-      {/* 步骤：1.描述（布局） —— 原型图 —— 2.配置（风格） —— 设计稿 —— 3.优化（交互）—— 重构稿 —— 4.编辑（页面）  */}
+      {/* 步骤：1.描述（结构） —— 原型图 —— 2.配置（风格） —— 设计稿 —— 3.配置（交互）—— 重构稿 —— 4.编辑（页面）  */}
       {
         stepsData.map((item, index) => <div
           key={item.id ?? index}
-          className={`height-100 overflow-hidden transition ${index < step ? 'border-right' : ''}`}
-          style={{ width: index < step ? '10vw' : step === index ? `${70 + (lastIndex - index) * 10}vw` : '0px' }}
+          className={`relative height-100 overflow-hidden transition ${index < step ? 'border-right' : ''}`}
+          style={{ width: index < step ? '120px' : step === index ? `calc(100vw - ${index * 120}px)` : '0px' }}
         >
           {item.element}
+          <div className={`absolute top-0 left-0 flex items-center justify-center width-100 height-100 bg-white color-gray-4 transition ${step === index ? 'opacity-0 events-none' : ''}`} onClick={() => setStep(index)}>{item.placeholder}</div>
         </div>)}
     </LandAffixContainer>
   )
