7f945a363df89583eed1bc0d52cf7479d08b4c59
minnasu <minnasu@tencent.com>
2025-03-25 20:41:48 +0800

    perf: 文章

--- apps/minna-profilo/src/pages/Articles/articles/Articles2025_2_2.tsx
+++ apps/minna-profilo/src/pages/Articles/articles/Articles2025_2_2.tsx
@@ -66,17 +66,33 @@ const Articles2025_2_2:React.FC = () => <ArticleLayout>
             </ArticleDesc>
         </ArticlePartLayout>
         <ArticlePartLayout>
-            <ArticleTitle type={'h2'} title={'三. 路径实现 Morph 变形动画的关键'}/>
+            <ArticleTitle type={'h2'} title={'三. 路径类型 Morph 变形动画的关键'}/>
             <ArticleDesc>Morph 动画是从一个图形逐渐变化到另一个图形，本质是图形的点与线在移动变化，两个图形能够互相过渡必须具备以下条件：
                 <ul>
                     <li>点数相同</li>
                     <li>起点与终点相同，方向相同</li>
-                    <li>两点之间的路径相同（例如都为贝塞尔曲线或都为直线）</li>
+                    <li>两点之间的路径相同（例如都为贝塞尔曲线或都为直线，对应代码中形同的绘制方式——L、H、C、Q）</li>
                 </ul>
             </ArticleDesc>
-            <ArticleImage url={'./images/articles2025/articles2025-2-2.png'} imgStyle={{width:'300px'}}></ArticleImage>
+            <ArticleImage url={'./images/articles2025/articles2025-2-2-1.png'}
+                          imgStyle={{width: '300px'}}></ArticleImage>
             <ArticleDesc>通常，在实现变形动画时拿到的两个图形并不满足以上两个条件，而是满足最少点数的原则，为了让两个图形实现变形效果，就需要手动将两个图形的点、线进行重绘，以满足变形动画的条件。</ArticleDesc>
-            <ArticleDesc></ArticleDesc>
+            <ArticleDesc>下面列举手动重绘的几种方式，实现调整点数与方向、直线转曲线：
+                <ul>
+                    <li>figma + 插件 fill rule Editor
+                        <ArticleImage url={'./images/articles2025/articles2025-2-2-2.png'}
+                                      className={'mt-12'}></ArticleImage>
+                    </li>
+                    <li>Illustrator</li>
+                </ul>
+            </ArticleDesc>
+            <ArticleDesc>转换之后将两个图形的 path 路径作为初始路径和结束路径，即可实现丝滑的 morph
+                变形动画。</ArticleDesc>
+            <ArticleDesc>
+                <div className={'mx-auto p-32'}>
+                    <StyledAnimationDemo4></StyledAnimationDemo4>
+                </div>
+            </ArticleDesc>
         </ArticlePartLayout>
     </ArticleSectionLayout>
 </ArticleLayout>
@@ -99,7 +115,7 @@ const StyledAnimationDemo2 = styled.div`
     width: 24px;
     height: 24px;
     background-color: var(--color-primary-2);
-    animation: morph2 2s linear infinite alternate;
+    animation: morph2 1s linear infinite alternate;
     @keyframes morph2 {
         0%{
             clip-path: polygon(
@@ -136,6 +152,20 @@ const StyledAnimationDemo3 = styled.div`
         }
         100%{
             clip-path: path('M15.5318 15.5297C15.8613 15.2002 16.131 14.8407 16.3507 14.4613C16.5705 14.0819 16.7303 13.6725 16.8401 13.2531C17.0598 12.4144 17.0499 11.5357 16.8301 10.7069C16.7203 10.2876 16.5505 9.88817 16.3308 9.49875C16.111 9.10934 15.8514 8.77983 15.5318 8.47029C15.2122 8.16076 14.8726 7.89116 14.5031 7.67149C14.1335 7.45182 13.714 7.28208 13.2946 7.17224C12.4456 6.94259 11.5568 6.94259 10.7078 7.17224C10.2884 7.28208 9.88886 7.45182 9.49935 7.67149C9.10984 7.89116 8.78026 8.15077 8.47065 8.47029C8.16104 8.78982 7.88139 9.14928 7.66167 9.51872C7.44194 9.88817 7.28214 10.2976 7.17228 10.7069C6.94257 11.5557 6.94257 12.4443 7.17228 13.2931C7.28214 13.7124 7.45193 14.1118 7.67165 14.5012C7.89138 14.8907 8.15105 15.2202 8.47065 15.5297C8.79025 15.8392 9.14979 16.1188 9.51933 16.3385C9.88886 16.5582 10.2983 16.7179 10.7078 16.8278C11.5568 17.0574 12.4456 17.0574 13.2946 16.8278C13.714 16.7179 14.1135 16.5482 14.5031 16.3285C14.8926 16.1088 15.2222 15.8492 15.5318 15.5297Z');
+        }
+    }
+`
+const StyledAnimationDemo4 = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: var(--color-primary-2);
+    animation: morph4 2s linear infinite alternate;
+    @keyframes morph4 {
+        0%{
+            clip-path: path('M49.5528 5L60.7785 39.5491L97.1057 39.5491L67.7164 60.9017L78.9421 95.4509L49.5528 74.0983L20.1636 95.4509L31.3893 60.9017L2 39.5491L38.3271 39.5491L49.5528 5Z');
+        }
+        100%{
+            clip-path: path('M49.5528 5L73.3292 22.2746L97.1057 39.5491L88.0239 67.5L78.9421 95.4509L49.5528 95.4509L20.1636 95.4509L11.0818 67.5L2 39.5491L25.7764 22.2746L49.5528 5Z')
         }
     }
 `
--- apps/minna-profilo/src/pages/Articles/components/ArticleImage.tsx
+++ apps/minna-profilo/src/pages/Articles/components/ArticleImage.tsx
@@ -5,13 +5,15 @@ type Props = {
     alt?: string;
     style?: CSSProperties;
     imgStyle?: CSSProperties;
+    className?: string;
 }
 const ArticleImage: React.FC<Props> = ({
                                            url = '正文内容...',
                                            alt,
                                            style,
-                                           imgStyle
-                                      }) => <div className={'flex justify-center width-100'} style={{maxHeight: '400px',...style}}>
+                                           imgStyle,
+    className=''
+                                      }) => <div className={`flex justify-center width-100 ${className}`} style={{maxHeight: '400px',...style}}>
     <img src={url} alt={alt} className={'radius-8'} style={{maxHeight: '400px',...imgStyle}} />
 </div>
 export default ArticleImage;
